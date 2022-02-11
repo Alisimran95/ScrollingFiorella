b75f52bb539ec8849ba9aafa855e7bfdbdcddacd
Alisimran95 <alisimranem@code.edu.az>
2022-02-11 13:43:57 +0400

    inspect change

--- FiorellaBackToFrontProject/FiorellaBackToFrontProject/wwwroot/js/script.js
+++ FiorellaBackToFrontProject/FiorellaBackToFrontProject/wwwroot/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(window).scroll(function() {
-        if ($(window).scrollTop() === $(document).height() - $(window).height()) {
+        if ($(document).scrollTop() >= $("#productRow").height() - $(".product-item").height()) {
             getData();
         }
     });
